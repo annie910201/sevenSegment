2e1fc93d7a6bd4ca638e92174099abc868b692c7
Annie <75851579+annie910201@users.noreply.github.com>
2021-10-22 19:01:15 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace SevenSeg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputHeight = new System.Windows.Forms.TextBox();
+            this.inputWidth = new System.Windows.Forms.TextBox();
+            this.sure = new System.Windows.Forms.Button();
+            this.inputNum = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "高";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "寬";
+            // 
+            // inputHeight
+            // 
+            this.inputHeight.Location = new System.Drawing.Point(55, 68);
+            this.inputHeight.Name = "inputHeight";
+            this.inputHeight.Size = new System.Drawing.Size(56, 25);
+            this.inputHeight.TabIndex = 2;
+            // 
+            // inputWidth
+            // 
+            this.inputWidth.Location = new System.Drawing.Point(55, 124);
+            this.inputWidth.Name = "inputWidth";
+            this.inputWidth.Size = new System.Drawing.Size(56, 25);
+            this.inputWidth.TabIndex = 4;
+            // 
+            // sure
+            // 
+            this.sure.Location = new System.Drawing.Point(16, 187);
+            this.sure.Name = "sure";
+            this.sure.Size = new System.Drawing.Size(95, 33);
+            this.sure.TabIndex = 5;
+            this.sure.Text = "確定";
+            this.sure.UseVisualStyleBackColor = true;
+            this.sure.Click += new System.EventHandler(this.sure_Click);
+            // 
+            // inputNum
+            // 
+            this.inputNum.Location = new System.Drawing.Point(16, 344);
+            this.inputNum.Name = "inputNum";
+            this.inputNum.Size = new System.Drawing.Size(100, 25);
+            this.inputNum.TabIndex = 6;
+            this.inputNum.TextChanged += new System.EventHandler(this.inputNum_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(13, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "請輸數字(-9~99)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(815, 442);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.inputNum);
+            this.Controls.Add(this.sure);
+            this.Controls.Add(this.inputWidth);
+            this.Controls.Add(this.inputHeight);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputHeight;
+        private System.Windows.Forms.TextBox inputWidth;
+        private System.Windows.Forms.Button sure;
+        private System.Windows.Forms.TextBox inputNum;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SevenSeg
+{
+    public partial class Form1 : Form
+    {
+        Button[,] btn1 = new Button[1, 1];//左邊的數字
+        Button[,] btn2 = new Button[1, 1];//右邊的數字
+        public Form1()
+        {
+            InitializeComponent();
+            inputNum.Enabled = false;
+        }
+        int width = 0;
+        int height = 0;
+        int clickTimes = 0;
+        private void sure_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                for(int i = 0; i < height; i++)
+                {
+                    for(int j = 0; j < width; j++)
+                    {
+                        this.Controls.Remove(btn1[i, j]);//開始之前先把之前的按鈕移除
+                        this.Controls.Remove(btn2[i, j]);
+                    }
+                }
+                
+                if (Convert.ToInt32(inputHeight.Text) < 7 || Convert.ToInt32(inputHeight.Text) > 15 || Convert.ToInt32(inputWidth.Text) < 5 || Convert.ToInt32(inputWidth.Text) > 10)
+                {
+                    MessageBox.Show("請輸入範圍內的數字", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    inputHeight.Clear();
+                    inputWidth.Clear();
+                }
+                else
+                {
+                    if (Convert.ToInt32(inputHeight.Text) % 2 == 0)
+                    {
+                        MessageBox.Show("高不能為偶數", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        inputHeight.Clear();
+                        inputWidth.Clear();
+                    }
+                    else
+                    {
+                        width = Convert.ToInt32(inputWidth.Text);
+                        height = Convert.ToInt32(inputHeight.Text);
+                        inputNum.Enabled = true;
+                        btn1 = resize(width, height, btn1);
+                        btn2 = resize(width, height, btn2);
+                        for (int i = 0; i < height; i++)
+                            {
+                                for (int j = 0; j < width; j++)
+                                {
+                                    int sizeY = 300 / height;
+                                    int sizeX = 220 / width;
+                                    btn1[i, j] = new Button();
+                                    btn2[i, j] = new Button();
+                                    btn1[i, j].SetBounds(125 + sizeX * j, 25 + sizeY * i, sizeX, sizeY);
+                                    btn2[i, j].SetBounds(360 + sizeX * j, 25 + sizeY * i, sizeX, sizeY);
+                                    btn1[i, j].BackColor = Color.White;
+                                    btn2[i, j].BackColor = Color.White;
+                                    Controls.Add(btn1[i, j]);
+                                    Controls.Add(btn2[i, j]);
+                                }
+                            }
+                        if (clickTimes != 0)
+                        {
+                            int num = Convert.ToInt32(inputNum.Text);
+                            if (num >=-9 && num <= 99)
+                            {
+                                if (num < 0)
+                                {
+                                    int left = 99;//隨便定的
+                                    Category(btn1, left);
+                                    int complement = 0 - num;
+                                    Category(btn2, complement);
+                                }
+                                else if (num < 10 && num > -1)
+                                {
+                                    int left = 0;
+                                    Category(btn1, left);
+                                    int right = num % 10;
+                                    Category(btn2, right);
+                                }
+                                else
+                                {
+                                    int left = num / 10; ;
+                                    Category(btn1, left);
+                                    int right = num % 10;
+                                    Category(btn2, right);
+                                }
+                            }
+                        }
+                        clickTimes++;
+                    }
+                }
+            }
+            catch (FormatException ){
+                MessageBox.Show("請輸入數字", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                inputHeight.Clear();
+                inputWidth.Clear();
+            }
+        }
+        Button[,] resize(int col,int row, Button[,] original)//Array.Resize只適用1-dimension，其他維度需要自行寫function
+        {
+            Button[,] newArray = new Button[row, col];
+            return newArray;
+        }
+        private void inputNum_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        btn1[i, j].BackColor = Color.White;//每次textBox讀到新的字串都要先變回白色button
+                        btn2[i, j].BackColor = Color.White;
+                    }
+                }
+                int num = Convert.ToInt32(inputNum.Text);
+                if (num <= 99 && num > -10)//分成左邊數字跟右邊數字各自判讀
+                {
+                    if (num < 0)
+                    {
+                        int left = 99;//隨便定的
+                        Category(btn1, left);
+                        int complement = 0 - num;
+                        Category(btn2, complement);
+                    }
+                    else if (num < 10 && num > -1)
+                    {
+                        int left = 0;
+                        Category(btn1, left);
+                        int right = num % 10;
+                        Category(btn2, right);
+                    }
+                    else
+                    {
+                        int left = num / 10; ;
+                        Category(btn1, left);
+                        int right = num % 10;
+                        Category(btn2, right);
+                    }
+                }
+                else//超出範圍，button回復白色
+                {
+                    for (int i = 0; i < height; i++)
+                    {
+                        for (int j = 0; j < width; j++)
+                        {
+                            btn1[i, j].BackColor = Color.White;
+                            btn2[i, j].BackColor = Color.White;
+                        }
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        btn1[i, j].BackColor = Color.White;
+                        btn2[i, j].BackColor = Color.White;
+                    }
+                }
+            }
+        }
+        int[] repre = new int[7] { 0,0,0,0,0,0,0 };//把數字分成七段去做區分
+        void Category(Button[,] button,int num)
+        {
+            for (int i = 0; i < 7; i++) repre[i] = 0;
+            switch (num)
+            {
+                case 0:
+                    for (int i = 0; i < 7; i++)
+                        if (i != 3) repre[i] = 1;
+                    break;
+                case 1:
+                    repre[2] = 1;repre[5] = 1;
+                    break;
+                case 2:
+                    for (int i = 0; i < 7; i++)
+                        if (i != 1&&i!=5) repre[i] = 1;
+                    break;
+                case 3:
+                    for (int i = 0; i < 7; i++)
+                        if (i != 1&&i!=4) repre[i] = 1;
+                    break;
+                case 4:
+                    for (int i = 0; i < 7; i++)
+                        if (i == 1 || i == 2||i==3||i==5) repre[i] = 1;
+                    break;
+                case 5:
+                    for (int i = 0; i < 7; i++)
+                        if (i != 2 && i != 4) repre[i] = 1;
+                    break;
+                case 6:
+                    for (int i = 0; i < 7; i++)
+                        if (i != 2 ) repre[i] = 1;
+                    break;
+                case 7:
+                    for (int i = 0; i < 7; i++)
+                        if (i == 0 || i == 2||i==5) repre[i] = 1;
+                    break;
+                case 8:
+                    for (int i = 0; i < 7; i++)
+                       repre[i] = 1;
+                    break;
+                case 9:
+                    for (int i = 0; i < 7; i++)
+                        if ( i != 4) repre[i] = 1;
+                    break;
+                case 99:
+                    repre[3] = 1;
+                    break;
+            }
+            if (repre[0] == 1)//最上面中間橫排
+                for (int j = 1; j < width - 1;j++) button[0, j].BackColor = Color.Blue;
+            if(repre[1]==1)//左上角直排
+                for(int j=1;j<height/2;j++)button[j,0].BackColor = Color.Blue;
+            if(repre[2]==1)//右上角直排
+                for (int j = 1; j < height / 2; j++) button[j, width-1].BackColor = Color.Blue;
+            if (repre[3] == 1)//中間橫排
+                for (int j = 1; j < width - 1; j++) button[height/2, j].BackColor = Color.Blue;
+            if (repre[4] == 1)//左下角直排
+                for (int j = 1; j < height / 2; j++) button[j+height/2, 0].BackColor = Color.Blue;
+            if (repre[5] == 1)//右下角橫排
+                for (int j = 1; j < height / 2; j++) button[j + height / 2 , width-1].BackColor = Color.Blue;
+            if(repre[6] == 1)//最下面中間橫排
+                for (int j = 1; j < width - 1; j++) button[height-1, j].BackColor = Color.Blue;
+        }
+    }
+}
